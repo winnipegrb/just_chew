331f3664197ab0f0187b18673b706ff1450e94fb
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-11-26 18:06:42 -0600

    Added focus and ignore tag filter support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -69,4 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Run all specs when everything is filtered
+  config.run_all_when_everything_filtered = true
 end
