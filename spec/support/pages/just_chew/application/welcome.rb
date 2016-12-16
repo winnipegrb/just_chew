a5875a6a2d3f502dc9e931dc7b4c059b8bb2f8f1
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-12-16 01:54:18 -0600

    Moved welcome page to just chew spec application module, added banner and card sections

---
+++ spec/support/pages/just_chew/application/welcome.rb
@@ -0,0 +1,17 @@
+require_relative '../application'
+
+module JustChew::Spec::Application::Welcome
+
+  class Banner < JustChew::Spec::Section
+  end
+
+  class Card < JustChew::Spec::Section
+  end
+
+  class Page < JustChew::Spec::Page
+    set_url %q(/)
+
+    section  :banner, Banner, '#main-banner'
+    sections :cards,  Card,   '#card'
+  end
+end
--- spec/support/pages/welcome_page.rb
+++
@@ -1,5 +0,0 @@
-class WelcomePage < SpecPage
-  set_url %q(/)
-
-  element :header, 'h1', text: 'Welcome'
-end
