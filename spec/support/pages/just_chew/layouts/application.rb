ad32d2f535e1f4d819e7a10c86a96abe94ca0fa4
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-12-16 22:32:34 -0600

    Added elements to application layout sections

--- spec/support/pages/just_chew/layouts/application.rb
+++ spec/support/pages/just_chew/layouts/application.rb
@@ -4,12 +4,23 @@ module JustChew::Spec::Layouts
     extend ActiveSupport::Concern
 
     class Header < SitePrism::Section
+      element  :logo,   '.navbar-brand img'
+      elements :items,  '.navbar-nav .nav-item'
+      element  :home,   '.navbar-nav .nav-link', text: 'Home'
+      element  :log_in, '.navbar-nav .nav-link', text: 'Log In'
     end
 
     class Footer < SitePrism::Section
 
       class SocialMedia < SitePrism::Section
+        elements :links,       '.navbar .nav-link'
+        element  :twitter,     '.navbar .nav-link .fa-twitter'
+        element  :facebook,    '.navbar .nav-link .fa-facebook'
+        element  :google_plus, '.navbar .nav-link .fa-google-plus'
       end
+
+      section :social_media, SocialMedia, '#social-media'
+      element :copy, '#copy'
     end
 
     included do
