a6e33083f080db54c1172fbf8064761228f163ca
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-12-24 16:41:25 -0600

    Made view expect failures more readable

--- spec/views/application/welcome_spec.rb
+++ spec/views/application/welcome_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'application/welcome.slim', type: :view do
 
+  before { allow(view).to receive(:inspect).and_return('view') }
+
   before do
-    expect(view).to receive(:welcome_cards).and_call_original
+    expect(view).to receive(:welcome_cards).with(no_args).and_call_original
     expect(view).to receive(:simple_form_for).and_call_original
   end
 
