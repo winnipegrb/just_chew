b20d4a6f82cd6889fd794a5dc02ce4edcd8f0619
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-13 00:16:46 -0500

    Generated profiles controller with #show and #update

---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ProfilesController, type: :controller do
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT #update" do
+    it "returns http success" do
+      put :update
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
