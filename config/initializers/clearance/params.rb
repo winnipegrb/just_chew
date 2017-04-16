993d1f6c464100c54261b15dd874d07536f24837
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-16 02:57:46 -0500

    Cleaned up clearance strong params patch

--- config/initializers/clearance/params.rb
+++ config/initializers/clearance/params.rb
@@ -1,14 +1,3 @@
 Clearance::UsersController.class_eval do
-
-  PARAMS = %i(first_name last_name email password)
-
-  private
-
-  def user_params
-    if action_name.to_sym.eql? :create
-      params.require(:user).permit(PARAMS)
-    else
-      Hash.new
-    end
-  end
+  include Clearance::UserParamable
 end
