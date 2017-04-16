76ccc5c63aad815efb54888d5ef1a64cae9947d7
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-16 02:59:52 -0500

    Added profiles#show tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ProfilesController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  let(:user) { create :user }
+
+  describe 'GET #show' do
+
+    context 'as guest' do
+
+      before { get :show }
+
+      it { should respond_with :redirect }
+
+      it { should redirect_to sign_in_path }
+    end
+
+    context 'as user' do
+
+      before do
+        sign_in_as user
+        get :show
+      end
+
+      it { should respond_with :success }
+
+      it { should render_template :show }
     end
   end
 
   describe "PUT #update" do
-    it "returns http success" do
-      put :update
-      expect(response).to have_http_status(:success)
-    end
+
   end
 
 end
