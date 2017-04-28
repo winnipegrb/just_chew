1e4ecf383119664e8c3ec1728ae12136427a3a57
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-27 22:42:19 -0500

    Added restaurants controller #index tests

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -2,10 +2,45 @@ require 'rails_helper'
 
 RSpec.describe RestaurantsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  let(:user) { create :user }
+
+  describe 'GET #index' do
+
+    let!(:restaurants)         { create_list :restaurant, 3 }
+    let(:assigned_restaurants) { assigns(:restaurants) }
+
+    before do
+      expect(Restaurant).to receive(:order)
+        .with(:updated_at).and_call_original
+    end
+
+    context 'as guest' do
+
+      before { get :index }
+
+      it { should respond_with :success }
+
+      it { should render_template :index }
+
+      it 'should assign restaurants as @restaurants' do
+        expect(assigned_restaurants).to match_array restaurants
+      end
+    end
+
+    context 'as user' do
+
+      before do
+        sign_in_as user
+        get :index
+      end
+
+      it { should respond_with :success }
+
+      it { should render_template :index }
+
+      it 'should assign restaurants as @restaurants' do
+        expect(assigned_restaurants).to match_array restaurants
+      end
     end
   end
 
