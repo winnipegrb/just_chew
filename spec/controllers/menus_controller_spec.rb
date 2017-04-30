25c0dfb1e7a2620e123cb1fb1f1731f17e0e88e5
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-30 12:54:40 -0500

    Added #show tests to menus controller spec

--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -2,10 +2,57 @@ require 'rails_helper'
 
 RSpec.describe MenusController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  let(:user) { create :user }
+
+  let!(:restaurant) { create :restaurant }
+
+  describe 'GET #show' do
+    let!(:menu)               { restaurant.menu }
+    let(:assigned_restaurant) { assigns(:restaurant) }
+    let(:assigned_menu)       { assigns(:menu) }
+
+    before do
+      expect(controller).to receive(:load_restaurant).and_call_original
+      expect(controller).to receive(:load_menu).and_call_original
+    end
+
+    context 'as guest' do
+
+      before do
+        get :show, params: { restaurant_id: restaurant.id }
+      end
+
+      it { should respond_with :success }
+
+      it { should render_template :show }
+
+      it 'should assign restaurant as @restaurant' do
+        expect(assigned_restaurant).to eq restaurant
+      end
+
+      it 'should assign menu as @menu' do
+        expect(assigned_menu).to eq menu
+      end
+    end
+
+    context 'as user' do
+
+      before do
+        sign_in_as user
+        get :show, params: { restaurant_id: restaurant.id }
+      end
+
+      it { should respond_with :success }
+
+      it { should render_template :show }
+
+      it 'should assign restaurant as @restaurant' do
+        expect(assigned_restaurant).to eq restaurant
+      end
+
+      it 'should assign menu as @menu' do
+        expect(assigned_menu).to eq menu
+      end
     end
   end
 
