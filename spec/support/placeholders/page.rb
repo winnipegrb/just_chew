892b67ebccb8c63c676002f91f4326a1995664f0
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-30 12:14:25 -0500

    Added restaurants page object

--- spec/support/placeholders/page.rb
+++ spec/support/placeholders/page.rb
@@ -2,4 +2,8 @@ placeholder :page do
   match /welcome/ do
     JustChew::Spec::Application::Welcome::Page
   end
+
+  match /restaurants/ do
+    JustChew::Spec::Restaurants::Index::Page
+  end
 end
