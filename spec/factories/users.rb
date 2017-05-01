22d600ba74e89be8e0197e043b39c2c7f013473d
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-01 01:41:17 -0500

    Updated user model with order_preference association, removed factory additions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,9 +8,5 @@ FactoryGirl.define do
     last_name  { Faker::Name.last_name }
     email
     password 'password'
-
-    after(:build) do |user, evaluator|
-      user.order_preference ||= build(:order_preference, user: user)
-    end
   end
 end
