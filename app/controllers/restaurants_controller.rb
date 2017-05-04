76047cdf78bf8ff912f542a67bfeda39f4ee5ecc
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-03 22:32:20 -0500

    Added restaurants_controller changes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,12 @@
-class RestaurantsController < ApplicationController
-  def index
+class RestaurantsController < OrderingController
+
+  before_action :load_restaurants
+
+  def index; end
+
+  private
+
+  def load_restaurants
+    @restaurants = Restaurant.order(:updated_at)
   end
 end
