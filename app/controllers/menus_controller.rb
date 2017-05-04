73cf2cbed9eaa5386238adf7a394841d9ab6c970
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-03 22:32:51 -0500

    Added menus_controller changes

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,17 @@
-class MenusController < ApplicationController
-  def show
+class MenusController < OrderingController
+
+  before_action :load_restaurant
+  before_action :load_menu
+
+  def show; end
+
+  private
+
+  def load_restaurant
+    @restaurant = Restaurant.find params[:restaurant_id]
+  end
+
+  def load_menu
+    @menu = @restaurant.menu
   end
 end
